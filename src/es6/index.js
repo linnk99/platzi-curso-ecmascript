8cc30c8ff179f0277ea011f487365be4251baab6
Hector Vega <linnk99@gmail.com>
2020-03-29 21:18:20 -0500

    Object destructuration, spread operator, let scope and constant features added

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,3 +1,4 @@
+// default param
 function newFunction(name, age, country) {
     var name = name || 'hector'
     var age = age || 30
@@ -13,6 +14,7 @@ function newFunction2(name = 'hector', age = 30, country = 'MX') {
 newFunction2()
 newFunction2('Ricardo', '23', 'CO')
 
+// concatenation
 let hello = 'Hello'
 let world = 'World'
 let epicPhrase = hello + ' ' + world
@@ -20,3 +22,51 @@ console.log(epicPhrase)
 
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2)
+
+// let y const multilinea
+let lorem = "Qui conseguatur. Commodi. Ipsum vel duis yet minima \n" + "Otra farase epica en multilinea"
+let lorem2 = `Otra frase epica que necesitamos.
+Ahora esta la frase en otra linea.
+`
+
+console.log(lorem)
+console.log(lorem2)
+
+// desestructuracion de objetos
+let person = {
+    'name': 'hector',
+    'age': 30,
+    'country': 'MX'
+}
+
+console.log(person.name, person.age)
+
+let {  name, age, country  } = person
+console.log(name, age, country)
+
+// spread operator
+let team1 = ['Oscar', 'Julian', 'Ricardo']
+let team2 = ['Valeria', 'Yesica', 'Camila']
+
+let education = ['David', ...team1, ...team2]
+
+console.log(education)
+
+var hola = 'hola'
+
+// let scope
+{
+    var globalVar = 'Global Var'
+}
+
+{
+    let globalLet = "Globar Let"
+}
+
+console.log(globalVar)
+console.log(globalLet)
+
+// const as constant values
+const a = 'b'
+a = 'a'
+console.log(a)
