ede759d640f7b5185ea563ae015fbf80acd5fcea
Hector Vega <linnk99@gmail.com>
2020-03-29 21:39:19 -0500

    Arrow functions and promises examples added

--- src/es6/index.js
+++ src/es6/index.js
@@ -70,3 +70,49 @@ console.log(globalLet)
 const a = 'b'
 a = 'a'
 console.log(a)
+
+// objetos mejorados
+let name = 'hector'
+let age = 30
+// es5
+obj = { name: name, age: age}
+// es6
+obj2 = { name, age}
+console.log(obj2)
+
+// arrow functions
+const names = [
+    { name: 'hector', age: 30 }
+    { name: 'yesica', age: 27 }
+]
+
+let listOfNames = names.map(function (item) {
+    console.log(item.name)
+})
+
+let listOfNames2 = names.map(item => console.log(item.name))
+const listOfNames3 = (ame, age, country) => {
+    ...
+}
+
+const listOfNames4 = name => {
+    ...
+}
+
+const square = num => num * num
+
+// promesas
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey!!')
+        } else {
+            reject('Oops!!')
+        }
+    })
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .then(() => console.log('hola'))
+    .catch(error => console.log(error))
